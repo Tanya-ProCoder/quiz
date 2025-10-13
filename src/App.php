86a4cdd8e0161d88b5f3b5a2593f50367af79532
Tanya-ProCoder <tanyabalda529@gmail.com>
2025-10-13 21:14:06 +0300

    new commit

--- src/App.php
+++ src/App.php
@@ -5,5 +5,16 @@ namespace Zver\Quiz;
 class App {
         public function __construct() {}
 
-        public function run() {}      
+        public function run() {
+                $response = [
+                        'question' => 'Какое дерево становится мокрым после дождя?',
+                        'answers' => [
+                                'Берёза',
+                                'Ель',
+                                'Яблоня',
+                                'Любое'
+                        ]
+                ];
+                echo json_encode($response);
+        }    
 }
