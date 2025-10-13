3c71b46f60ea6b3652a50f0cabe5aeb37522a55a
Tanya-ProCoder <tanyabalda529@gmail.com>
2025-10-13 19:40:12 +0300

    add some style

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -12,6 +12,23 @@ export default function App() {
                     </div>
                 </div>
             </nav>
+            <div className="container">
+                <div className="row justify-content-center">
+                    <div className="col-md-8">
+                        <div className="mt-4 paper">
+                            <div className="card-header text-center">
+                                <h3>Вопрос 1</h3>
+                            </div>
+                            <div className="card-body p-5 text-center">
+                                <div className="mb-3 p-3 border rounded">Вариант 1</div>
+                                <div className="mb-3 p-3 border rounded">Вариант 2</div>
+                                <div className="mb-3 p-3 border rounded">Вариант 3</div>
+                                <div className="mb-3 p-3 border rounded">Вариант 4</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
